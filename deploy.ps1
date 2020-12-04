28e7f8d2dc834a57c8b875162f878a03141ed0e7
Jason Racey <jasonracey@gmail.com>
2020-12-04 04:00:26 -0800

    Fix deployment script

--- deploy.ps1
+++ deploy.ps1
@@ -1,2 +1,3 @@
+$env:Path += ";C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin"
 dotnet build --configuration Release
 xcopy /I /Y .\Keyworder\bin\Release\netcoreapp3.1 ..\..\Tools\Keyworder /EXCLUDE:EXCLUDE
